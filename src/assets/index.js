9fd458c5196d70d6931586aca5593e603b7e0f8b
PradeepSinghCoder <pspsingh000@gmail.com>
2025-09-08 11:14:00 +0530

    Add files via upload
    first

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+import logo from "./profilepic.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import nodejs from "./tech/nodejs.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import unity from "./tech/unity.png";
+import unreal from "./tech/unreal.svg";
+import cocos from"./tech/cocos.png";
+import cloud from"./tech/cloud.png";
+import csharp from"./tech/csharp.png";
+import python from "./tech/python.png";
+import reactjs from "./tech/reactjs.png";
+
+import passiongaming from "./company/passiongaming.jpg";
+import t7gaming from "./company/t7gaming.png";
+import tooliqa from "./company/tooliqa.png";
+import appsoleutlogo from "./company/appsoleutlogo.svg";
+import vimap from "./company/vimaplogo.jpg";
+import oodles from "./company/oodles.png";
+
+
+import create from "./create.png";
+import xplore from "./xplore.png";
+import holoprojector from "./holoprojector.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+
+  figma,
+  git,
+  nodejs,
+  reactjs,
+  typescript,
+  threejs,
+  unity,
+  unreal,
+  python,
+  cloud,
+  cocos,
+  csharp,
+
+  create,
+  xplore,
+  holoprojector,
+  passiongaming,
+  oodles,
+  t7gaming,
+  tooliqa,
+  appsoleutlogo,
+  vimap
+};
