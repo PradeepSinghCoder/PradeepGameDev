f8124f49ef7ec11d415aa3144f4a7a268b768b88
bbdevproject <consultant1@vivirooms.com>
2025-09-08 12:28:52 +0530

    all data

---
+++ src/components/About.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Tilt from 'react-parallax-tilt';
+import {motion} from 'framer-motion'
+import { styles } from '../styles';
+import { services } from '../constants/constants';
+import {fadeIn,textVariant} from '../utils/motion'
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({index,title,icon})=>{
+  return(
+      <Tilt className='xs:w-[200px] w-full'>
+          <motion.div variants= {fadeIn('right','spring', 0.5*index,1,0.75)}
+          className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+          >
+          <div options = {
+            {
+              max:45,
+              scale:1,
+              speed:450
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-5 min-h-[180px] flex justify-evenly items-center flex-col'
+          >
+            <img src={icon} alt={title}
+            className='w-16 h-16 object-contain'/>
+            <h3 className='text-white text-[20]px font-bold text-center'>{title}</h3>
+
+
+          </div>
+          </motion.div>
+      </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+    <motion.div variants={textVariant()}> 
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
+    </motion.div>
+
+    <motion.p variants={fadeIn("","",0.1,1)}
+    className='mt-4 text-secondary text-[17px]
+    max-w-3xl leading-[30px]'>
+      I'm a dynamic Senior Unity Developer with over 8 years of hands-on experience building high-performance 2D/3D games, AR/VR/XR/Metaverse applications, and WebGL modules. Expert in C# scripting, real-time computational geometry, and shader graph optimization. Proven track record delivering end-to-end solutions—from concept and prototyping to launch—while collaborating cross functionally and mentoring teams. Passionate about pushing interactive experiences forward, I aim to step into a lead architect role where I can drive innovation in immersive gaming and expand my expertise in multiplayer and AI-driven gameplay.
+    </motion.p>
+
+    <div className = 'mt-20 flex flex-wrap gap-10'>
+      {
+        services.map((service,index)=>(
+          <ServiceCard key = {service.title} index = {index} {...service}/>
+        ))
+      }
+    </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About,'about')
---
+++ src/constants/constants.js
@@ -0,0 +1,298 @@
+import
+{
+  mobile,
+  backend,
+  creator,
+  web,
+  unity,
+  unreal,
+  csharp,
+  threejs,
+  cocos,
+  cloud,
+  typescript,
+  python,
+  nodejs,
+  git,
+  figma,
+  passiongaming,
+  tooliqa,
+  t7gaming,
+  oodles,
+  appsoleutlogo,
+  vimap,
+  holoprojector,
+  create,
+  xplore
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Unity 2D-3D Games Developer",
+    icon: web,
+  },
+  {
+    title: "AR App Developer",
+    icon: mobile,
+  },
+  {
+    title: "VR App Developer",
+    icon: backend,
+  },
+  {
+    title: "XR Apps Developer",
+    icon: creator,
+  },
+  {
+    title: "Metaverse Apps Developer",
+    icon: creator,
+  },
+  {
+    title: "WebGL Apps/Games Developer",
+    icon: creator,
+  }
+];
+
+const technologies = [
+  {
+    name: "Unity",
+    icon: unity,
+  },
+  {
+    name: "Unreal",
+    icon: unreal,
+  },
+  {
+    name: "C#",
+    icon: csharp,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "Cocos Creator",
+    icon: cocos,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+
+  {
+    name: "Python",
+    icon: python,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "cloud",
+    icon: cloud,
+  },
+];
+
+const experiences = [
+  {
+    title: "Sr. Unity Developer",
+    company_name: "Passion Gaming Private Limited",
+    icon: passiongaming,
+    iconBg: "#FFFFFF",
+    date: "Mar 2024 - Present",
+    points: [
+        "Optimized C# code and asset pipelines, slashing bug rates by 30% and cutting iteration times by 25%.",
+        "Orchestrated sprint planning, task distribution, and daily stand-ups for a 5-member team, driving a 20% uplift in on-time feature delivery.",
+        "Implemented user-analytics tracking to surface top-used features, then leveraged insights to refine UI/UX—boosting average session length by 15%.",
+        "Redesigned in-game interfaces with richer interactions and animations, elevating user satisfaction and engagement.",
+        "Streamlined build size through asset compression and scene culling, reducing download footprint by 35% and improving load times."
+
+    ],
+  },
+  {
+    title: "Sr. Unity Developer",
+    company_name: "T7 Gaming Private Limited",
+    icon: t7gaming,
+    iconBg: "#FFFFFF",
+    date: "Aug 2023 - Dec 2023",
+    points: [
+     "Led a 7-member team, owning sprint planning, task breakdown, and daily stand-ups to boost on-time delivery by 25%.",
+    "Rapidly prototyped 5+ AR and game concepts—using Unity, AR Foundation, and Vuforia—to validate client ideas in under one week.",
+    "Architected and implemented core features for a U.S. land-based casino slot title, including Horizontal/Vertical Dancing Pots and Expanding Wilds, elevating player engagement.",
+    "Collaborated with QA and design leads to refine game mechanics and ensure seamless transitions between land-based and digital gameplay.",
+    "Developed of immersive VR prototypes using Unity and SteamVR, tailored for client demos and concept validation."
+
+],
+  },
+  {
+    title: "Sr. Lead Development",
+    company_name: "Oodles Technologies Private Limited",
+    icon: oodles,
+    iconBg: "#FFFFFF",
+    date: "Feb 2020 – Aug 2023",
+    points: [
+    "Orchestrated sprint planning, task allocation, and code reviews for a 8-member Unity team, upholding 95%+ code-quality standards and on-time delivery of WebGL modules and game prototypes.",
+    "Architected and integrated a scalable Unity WebGL visualization module into the flagship web platform, consolidating real-time data from multiple internal services and backend APIs.",
+    "Designed data-processing pipelines that transformed raw payloads into interactive 2D/3D charts, heatmaps, and spatial visualizations—elevating user exploration and engagement.",
+    "Built a robust communication layer with WebSockets and postMessage, enabling seamless, bidirectional data flow and autonomous control across all web-app modules.",
+    "Optimized asset streaming and bundle management, slashing initial load times by 40% while maintaining a consistent 60 FPS rendering performance under heavy data loads.",
+    "Led creation of multiple game and demo prototypes—showcasing new interaction patterns and accelerating client R&D cycles by 50%.",
+    "Delivered enterprise-grade VR applications for architectural walkthroughs and product visualization using Unity, Oculus SDK, and HTC Vive."
+
+    ],
+  },
+  {
+    title: "Unity 3D Developer",
+    company_name: "Appsoleut Coders LLP",
+    icon: appsoleutlogo,
+    iconBg: "#FFFFFF",
+    date: "June 2017 – April 2019",
+    points: [
+    "Engineered realistic car physics and AI-controlled vehicles for free-roam 3D racing games, enhancing gameplay depth and immersion.",
+    "Collaborated with 2D/3D artists to create optimized game-ready assets, reducing scene load times and improving frame rates across devices.",
+    "Integrated Firebase for analytics and Crashlytics for real-time crash tracking, enabling faster issue resolution and stability improvements.",
+    "Developed asset streaming via custom APIs, allowing direct downloads from the server—cutting game size by 40% and improving scalability.",
+    "Implemented in-game ad monetization strategies, including dynamic ad placements in gameplay zones to maximize revenue without disrupting UX.",
+    "Built reusable Unity plugins and integrated third-party SDKs to accelerate development cycles and expand feature capabilities.",
+    "Delivered multiple simulation-based titles with polished environments, interactive physics, and modular gameplay systems.",
+    ],
+  },
+
+  {
+    title: "Unity 3D Developer",
+    company_name: "ViMAP Services Private Limited",
+    icon: vimap,
+    iconBg: "#FFFFFF",
+    date: "May 2016 – June 2017",
+    points: [
+    "Gained hands-on mastery of Unity engine and C# scripting by building 2D, 3D, and physics-driven games across Android, iOS, PC, WebGL, and Windows Mobile platforms.",
+    "Developed hyper-casual and simulation titles with optimized physics systems, interactive gameplay loops, and responsive controls.",
+    "Implemented native plugins and libraries for Android and iOS to extend platform-specific functionality and streamline deployment.",
+    "Integrated Firebase for analytics and crash reporting, along with in-app purchases and social login features to enhance monetization and user retention.",
+    "Collaborated with artists and designers to deliver polished, lightweight builds with efficient asset pipelines and scalable architecture"
+
+    ],
+  },
+
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Twinn Create",
+    description: "The end to end solution for Architects, from Floor Plan Creation to photorealistic rendering stage in real-time 3D",
+    tags: [
+      {
+        name: "unity",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "csharp",
+        color: "green-text-gradient",
+      },
+      {
+        name: "hlsl",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: create,
+    source_code_link: "https://twinn.pro/",
+  },
+  {
+    name: "XPLORE Journey Byjus",
+    description:
+      "Xplore - part of BYJU’S App, was introduced to make the learning concepts fun, engaging and personalised for everyone",
+    tags: [
+      {
+        name: "unity",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "cocos-creator",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: xplore,
+    source_code_link: "https://www.youtube.com/watch?v=LtVm4Hsrp2E",
+  },
+  {
+    name: "Holoprojector Apps",
+    description:
+      "HoloProjector not only provides you with a holographic display but also lets the user interact with the content in 3D space.",
+    tags: [
+      {
+        name: "unity",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "kinect",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ik",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: holoprojector,
+    source_code_link: "https://www.holoworld.one/holoprojector",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
